bbe1a7dcd04db0d272f0b7033258ba98cb20bddd
Ivan Bertona <ibrt@users.noreply.github.com>
2018-12-17 23:26:42 +0100

    Update golang-tools to 1.0.3

--- Formula/golang-tools.rb
+++ Formula/golang-tools.rb
@@ -1,9 +1,9 @@
 class GolangTools < Formula
   desc "Useful macOS tools for the savvy Go programmer."
   homepage "https://github.com/ibrt/golang-tools"
-  url "https://github.com/ibrt/golang-tools/archive/1.0.2.tar.gz"
-  version "1.0.2"
-  sha256 "cf71eb74e6d197a88765733b3dd75e347228e7e6a9ef2fd6536d54f039ba5505"
+  url "https://github.com/ibrt/golang-tools/archive/1.0.3.tar.gz"
+  version "1.0.3"
+  sha256 "735ea417bc4c1f12c517764d09596e26b75fd6d64f99ae8cd867667866749030"
 
   def install
     bin.install "tools/gobin"
